27e00d597d92de25d6ca22c4057a0852b8d40bcb
ipool-research <rd.acustica.ipool@gmail.com>
2025-10-08 12:25:37 +0200

    Update main.py con automazioni

--- src/video/main.py
+++ src/video/main.py
@@ -1,34 +1,39 @@
 import os
 import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
 from VIDEO import Video_processing
 from MODELS import ModelProcessor
 from INDEX import Index
 from FRAMEEXTRACTION import GoProH265FastExtractor
 from PROCESSOR import AmmaloramentiProcessor
+root = tk.Tk()
+root.withdraw()
 
-cartella_video = "video" # percorso della cartella che contiene il video da elaborare
-path = r"/home/ipool/Desktop/main_scheda" 
-frames = r"/home/ipool/Desktop/main_scheda/"+cartella_video+"/frames_estratti"
+path = tk.filedialog.askdirectory(title = "Seleziona cartella di lavoro") 
+path_cartella_video = tk.filedialog.askdirectory(title = "Seleziona cartella video")
+cartella_video = os.path.basename(os.path.normpath(path_cartella_video)) # percorso della cartella che contiene il video da elaborare
+frames = os.path.join(path_cartella_video, "frames_estratti")
 
 os.makedirs(frames, exist_ok=True)
 
 def main_function(cartella):        #Nome della cartella con tutti i video dentro
-    cartella_input = os.path.join(path, cartella)
 
-    model_class =ModelProcessor(cartella_input, 'yolo')
+    model_class =ModelProcessor(cartella, 'yolo')
     index_class = Index(frames)
-    csv_folder = os.path.join(cartella_input, 'csv')
+    csv_folder = os.path.join(cartella, 'csv')
     os.makedirs(csv_folder, exist_ok=True)
-    audio_gps_path = r"/home/ipool/Desktop/output_audio/GPS_classe_acustica.csv"
-    video_class = Video_processing(path)
-    
 
+    audio_gps_path = tk.filedialog.askopenfilename(title= "Selezionare file di output dell'analisi audio")
+    
+    video_class = Video_processing()
+    
     video_class.set_dir(cartella) 
 
     speed = []
     durata = [0]
 
-    file_mp4 = [f for f in os.listdir(cartella_input) if f.lower().endswith(".mp4")]
+    file_mp4 = [f for f in os.listdir(cartella) if f.lower().endswith(".mp4")]
 
     for file in file_mp4:
         # Nome senza estensione
@@ -36,8 +41,8 @@ def main_function(cartella):        #Nome della cartella con tutti i video dentr
         # Path sottocartella
         video_class.estrai_gpx(nome_senza_ext)
         video_class.extract_csv(nome_senza_ext)
-        patt = os.path.join(cartella_input,'csv', nome_senza_ext + '-gps.csv')
-        pat2 = os.path.join(cartella_input, file)
+        patt = os.path.join(cartella,'csv', nome_senza_ext + '-gps.csv')
+        pat2 = os.path.join(cartella, file)
         vel, fps = video_class.ricalcolo_velocità(patt , pat2)
         speed.extend(vel)
         durata.append(len(vel))
@@ -47,7 +52,7 @@ def main_function(cartella):        #Nome della cartella con tutti i video dentr
     extraction_points = video_class.calculate_frame_extraction_points(speed)
 
     extractor = GoProH265FastExtractor(
-        video_folder=cartella_input,
+        video_folder=cartella,
         video_limits=durata,  # dev’essere una lista come [0, n1, n2, ...]
         extraction_points=extraction_points,
         output_folder=frames,
@@ -56,7 +61,7 @@ def main_function(cartella):        #Nome della cartella con tutti i video dentr
         crop_size=1080
     )
     extractor.run()
-    
+
     df = pd.read_csv(audio_gps_path)
 
     # Legge la prima riga della colonna desiderata (es. colonna 'Nome')
@@ -66,7 +71,7 @@ def main_function(cartella):        #Nome della cartella con tutti i video dentr
     model_class.run_all_folders_filtered()
 
     index_class.process_all_folders_parallel()
-    processor = AmmaloramentiProcessor(cartella_input,audio_gps_path) 
+    processor = AmmaloramentiProcessor(cartella,audio_gps_path) 
     processor.run()
 
     #create map
@@ -75,4 +80,4 @@ def main_function(cartella):        #Nome della cartella con tutti i video dentr
 if __name__ == "__main__":
     import multiprocessing
     multiprocessing.freeze_support()  
-    main_function(cartella_video)
+    main_function(path_cartella_video)
