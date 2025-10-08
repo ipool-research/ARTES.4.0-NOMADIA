1a04653cebc149aea04eb2d1bfad6a4168aea19f
ipool-research <rd.acustica.ipool@gmail.com>
2025-10-08 12:17:50 +0200

    Update avvio_misura.sh

--- src/script/avvio_misura.sh
+++ src/script/avvio_misura.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Script per avviare l'applicativo Artes Audio
 
-# Spostati nella cartella di progetto
-cd ~/Desktop/Artes_audio || { echo "Cartella non trovata!"; exit 1; }
+# Spostati nella cartella dell'ambiente virtuale
+cd ../Artes_Audio || { echo "Cartella non trovata!"; exit 1; }
 
 # Attiva l'ambiente virtuale
-source artes_env/bin/activate || { echo "Impossibile attivare l'ambiente virtuale!"; exit 1; }
+source artes_audio/bin/activate || { echo "Impossibile attivare l'ambiente virtuale!"; exit 1; }
+
+# Spostati nella cartella di lavoro
+cd ../audio
 
 # Avvia lo script Python principale
 python main.py
